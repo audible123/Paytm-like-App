9fe218a50864cde0b5f4e5cf10cddfe323fcf837
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-04-19 18:29:08 +0530

    Update Users.jsx

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -10,7 +10,7 @@ export const Users = () => {
   useEffect(() => {
     axios
       .get(
-        "http://localhost:3000" + "/api/v1/user/bulk?filter=" + filter
+        "https://paytm-like-app-6cs3.onrender.com" + "/api/v1/user/bulk?filter=" + filter
       )
       .then((response) => {
         setUsers(response.data.user);
