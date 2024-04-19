41b67ce1ddf272b122f6092c2b0bbec54c7e96bb
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-04-19 18:28:17 +0530

    Update Appbar.jsx

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -15,7 +15,7 @@ export const Appbar = () => {
       navigate("/signin"); // Redirect to sign-in page if token doesn't exist
     } else {
       axios
-        .get("http://localhost:3000" + "/api/v1/user/getUser", {
+        .get("// https://paytm-like-app-6cs3.onrender.com" + "/api/v1/user/getUser", {
           headers: {
             Authorization: "Bearer " + localStorage.getItem("token"),
           },
