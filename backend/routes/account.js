5837de3faa8dc2ebd02e3cdd55e83f9c2eebf618
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-04-29 21:47:09 +0530

    Update account.js 3

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -69,12 +69,20 @@ router.post("/transfer", authMiddleware, async (req, res) => {
     recipientAccount.balance += amount;
     await recipientAccount.save({ session });
 
-    // Create transaction record for the sender
+    // Create transaction record
     const transaction = await Transaction.create({
       senderId: req.userId,
       recipientId: to,
       amount,
       transactionType: 'sent', // Marking the transaction as sent
+    });
+
+    // Create a new transaction record for the recipient to indicate receiving
+    await Transaction.create({
+      senderId: req.userId,
+      recipientId: to,
+      amount,
+      transactionType: 'received', // Marking the transaction as received
     });
 
     // Commit Transaction
@@ -106,7 +114,7 @@ router.get("/transactions", authMiddleware, async (req, res) => {
 
     // Map over transactions to add transactionType field
     const transactionsWithTypes = transactions.map(transaction => ({
-      ...transaction.toObject(), // Convert Mongoose document to JavaScript object
+      ...transaction._doc,
       transactionType: transaction.senderId.toString() === req.userId ? 'sent' : 'received',
     }));
 
